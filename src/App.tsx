1ffb69b8e7f1ed9c7779ebabae10f0b5f4ac706c
thejam2 <thejam2@naver.com>
2023-04-20 17:43:04 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+
+const Head = styled.div`
+  color: #e7473c;
+  font-family : RingsideExtraWide-Book,Blank,Helvetica Neue,Helvetica,sans-serif;
+ `;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Head>
+        <h1>BEST HORROR SCENES</h1>
+        <div className="bottom">
+          <h3>An ever growing collection featuring some of the best scenes in horror.</h3>
+          <p>“Best Horror Scenes” is a collection of scenes I feel are some of the most affecting in horror. Some may be simple black cat scares, others may be more subdued or nuanced. Many come from films that aren't necessarily “horror” but have elements or threads of horror.</p>
+          <div className="bottomgray">
+            <ul>
+              <li>Newsletter</li>
+              <li>Suggest a Scene</li>
+              <li>YouTube</li>
+              <li>Twitter</li>
+              <li>Contact</li>
+              <li>RSS</li>
+              <li>Letterboxd</li>
+            </ul>
+            <p>
+              Best Horror Scenes is a love letter to the Horror film genre by Brandon Durham. Most film clips contain major spoilers, and it is recommended you don’t watch the clip if you plan on seeing the film.
+            </p>
+          </div>
+        </div>
+      </Head>
     </div>
   );
 }
